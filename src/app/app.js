9d4317f9da280c77287e0474c36735983a56067f
kritikgarg <kritikgarg240194@gmail.com>
2016-10-17 10:39:22 +0530

    ui for timesheet

--- src/app/app.js
+++ src/app/app.js
@@ -15,5 +15,18 @@ angular.module('BlurAdmin', [
 
   'BlurAdmin.theme',
   'BlurAdmin.pages'
-]);
+]).run(checkAccessOnStateChange);
+
 console.log("app.js is loaded");
+
+
+function checkAccessOnStateChange($rootScope,$location) {
+    $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+        console.log("works fine");
+        $location.path('../auth.html');
+        //$state.transitionTo('dashboard');
+        event.preventDefault();
+        //$state.go('dashboard');
+
+    });
+}
